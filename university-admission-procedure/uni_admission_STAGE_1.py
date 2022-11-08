19de204241550a0308d9da6bd3196926c03b4907
Rozalia Przychodzka <107406800+Rossalieee@users.noreply.github.com>
2022-11-08 21:53:43 +0100

    Create uni_admission_STAGE_1.py

---
+++ university-admission-procedure/uni_admission_STAGE_1.py
@@ -0,0 +1,8 @@
+exam_1 = int(input())
+exam_2 = int(input())
+exam_3 = int(input())
+
+exam_sum = exam_1 + exam_2 + exam_3
+mean = exam_sum / 3
+print(mean)
+print("Congratulations, you are accepted!")
